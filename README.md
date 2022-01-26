8869a5190f14386635512ef71fba5a616eb1caf0
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-01-26 17:39:47 +0800

    ssadsa

--- README.md
+++
@@ -1,6 +0,0 @@
-# 工程简介
-## 基于 Nacos + SpringCloud Alibaba
-
-
-# 延伸阅读
-
--- src/main/java/com/practice/mapper/ClientMapper.java
+++
@@ -1,9 +0,0 @@
-package com.practice.mapper;
-
-import com.practice.entity.Client;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientMapper extends BaseMapper<Client> {
-}
