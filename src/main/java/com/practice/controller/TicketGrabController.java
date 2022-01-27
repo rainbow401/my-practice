0c12b6ad994a1afb9687271291d4e4264ffb0a65
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-01-27 13:58:03 +0800

    方案增加 kafka 存入mysql做持久化

--- src/main/java/com/practice/controller/TicketGrabController.java
+++ src/main/java/com/practice/controller/TicketGrabController.java
@@ -1,21 +1,24 @@
 package com.practice.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.practice.entity.TicketTel;
 import com.practice.mapper.ClientMapper;
+import com.practice.mapper.TicketTelMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author yanzhihao
- * @Description:
+ * @Description: 抢票实践
  * @date 2021/12/11 3:04 下午
  */
 @RestController
@@ -23,16 +26,29 @@ import java.util.Set;
 @Slf4j
 public class TicketGrabController {
 
+
+    private List<String> repeatTel = new ArrayList<>();
+
     @Autowired
     private ClientMapper clientMapper;
 
+    @Autowired
+    private TicketTelMapper ticketTelMapper;
+
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     @GetMapping("/init")
     public Object initSet() {
 
         Long start = System.currentTimeMillis();
+        redisTemplate.delete("ticket");
+        redisTemplate.delete("tel");
+        ticketTelMapper.delete(new QueryWrapper<>());
+        repeatTel.clear();
 
         Set<String> tel = new HashSet<>();
         for (int i = 0; i < 5000; i++) {
@@ -42,57 +58,62 @@ public class TicketGrabController {
 
         Long end = System.currentTimeMillis();
         log.info("耗时：{}", end - start);
+
         return redisTemplate.boundSetOps("ticket").members();
     }
 
-    @GetMapping("ticket")
-    public Object getTicket() {
-        GetTicket getTicket = new GetTicket();
-        Thread getTicketThread = new Thread(getTicket);
-        getTicketThread.start();
-        return redisTemplate.boundSetOps("tel").members();
-    }
+    @GetMapping("/ticket")
+    public Object getTicket(@RequestParam("tel") String tel) {
+        log.info("======时间： {}", System.currentTimeMillis());
 
-    public class GetTicket implements Runnable {
-        @Override
-        public void run() {
-            int startTel = 1000;
-            for(int tel = startTel; tel < startTel + 5000; tel++) {
-                //首先将手机号放入抢到的手机号Set里
-                Long success = redisTemplate.boundSetOps("tel").add(tel);
-                log.info("tel:{} success:{}}", tel,success);
-                //如果success 返回 0 则表示手机号已存在
-                if(success == null ||success == 1L) {
-                    Object getSuccess = redisTemplate.boundSetOps("ticket").pop();
-                    log.info("tel:{} getSuccess:{}}", tel,getSuccess);
+        //首先将手机号放入抢到的手机号Set里
+        Long success = redisTemplate.boundSetOps("tel").add(tel);
+        log.info("tel:{} success:{}}", tel, success);
 
-                    //如果 getSuccess 为 null 则表示票已抢完
-                    if(getSuccess != null) {
-                        log.info("{}-抢票成功", tel);
-                    } else {
-                        //票已抢完 需要将存放已抢完手机号的Set里的手机号删除
-                        Long removeSuccess = redisTemplate.boundSetOps("tel").remove(tel);
-                        if(removeSuccess == null || removeSuccess != 1L) {
-                            log.info("{}移除失败 ", tel);
-                            //在另外的地方存储
-                        }
-                    }
-                } else {
-                    log.info("{} 已抢到票 ", tel);
+        //如果success 返回 0 则表示手机号已存在
+        if (success == null || success == 1L) {
+
+            Object getSuccess = redisTemplate.boundSetOps("ticket").pop();
+            log.info("tel:{} getSuccess:{}}", tel, getSuccess);
+
+            //如果 getSuccess 为 null 则表示票已抢完
+            if (getSuccess != null) {
+                log.info("{}-抢票成功", tel);
+                //放到kafka队列中
+                kafkaTemplate.send("ticket", tel);
+            } else {
+                //票已抢完 需要将存放已抢完手机号的Set里的手机号删除
+                Long removeSuccess = redisTemplate.boundSetOps("tel").remove(tel);
+                if (removeSuccess == null || removeSuccess != 1L) {
+                    log.info("{}移除失败 ", tel);
+                    //TODO 在另外的地方存储
                 }
             }
+        } else {
+            repeatTel.add(tel);
+            log.info("{} 已抢到票 ", tel);
         }
+        return "";
     }
 
-    @GetMapping("/check")
+    @KafkaListener(topics = {"ticket"})
+    public void consumer(String tel) {
+        log.info("消费：{} ", tel);
+        TicketTel ticketTel = new TicketTel();
+        ticketTel.setTel(tel);
+        ticketTel.setStatus(1);
+        ticketTelMapper.insert(ticketTel);
+    }
+
+    @GetMapping("/ticket/count")
     public Object checkTicket() {
-//        log.info("tel: {}", redisTemplate.boundSetOps("tel").members());
-//        log.info("telCount: {}", redisTemplate.boundSetOps("tel").members().size());
-//        log.info("ticketCount: {}", redisTemplate.boundSetOps("ticket").members().size());
         HashMap<String, Object> result = new HashMap<>();
         result.put("tel", redisTemplate.boundSetOps("tel").members());
         result.put("telCount", redisTemplate.boundSetOps("tel").members().size());
         result.put("ticketCount", redisTemplate.boundSetOps("ticket").members().size());
+        result.put("repeatTel", repeatTel);
+        result.put("repeatTelCount", repeatTel.size());
+        result.put("ticketTel", ticketTelMapper.selectList(new QueryWrapper<>()).size());
         return result;
     }
 
---
+++ src/main/java/com/practice/mapper/TicketTelMapper.java
@@ -0,0 +1,9 @@
+package com.practice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.practice.entity.TicketTel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketTelMapper extends BaseMapper<TicketTel> {
+}
