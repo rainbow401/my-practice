1828a700b008f8e8fca1169cf7924c26ff7e0573
ZR21080178-闫志浩 <yanzhihao@zhongruigroup>
2022-02-09 15:07:54 +0800

    Update RedisConfig.java

--- src/main/java/com/practice/config/RedisConfig.java
+++ src/main/java/com/practice/config/RedisConfig.java
@@ -18,14 +18,14 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
         template.setConnectionFactory(factory);
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new
-                Jackson2JsonRedisSerializer(Object.class);
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
-        StringRedisSerializer stringRedisSerializer = new
-                StringRedisSerializer();
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
         // key采用String的序列化方式
         template.setKeySerializer(stringRedisSerializer);
         // hash的key也采用String的序列化方式
