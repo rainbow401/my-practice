668502de9b53c6e47c8fd2e8d30b522ceba72bc5
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-03-14 08:44:58 +0800

    update

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -4,10 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @MapperScan
 @Slf4j
 public class PracticeApplication {
