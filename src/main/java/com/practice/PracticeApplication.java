d7d7d63bd9da0dd7fb7a9d99742f5e2cc63963f8
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-07-22 10:51:55 +0800

    mail

--- src/main/java/com/practice/PracticeApplication.java
+++ src/main/java/com/practice/PracticeApplication.java
@@ -1,15 +1,9 @@
 package com.practice;
 
-import com.practice.vo.AccessTokenVO;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @MapperScan("com.practice.mapper")
@@ -17,12 +11,6 @@ import java.util.Arrays;
 public class PracticeApplication {
 
     public static void main(String[] args) {
-        System.out.println("--------------------------");
-        for (String e : args) {
-            System.out.println("args: " + e);
-        }
-        System.out.println("--------------------------");
-        args = new String[] {"--server.port=9090"};
         SpringApplication.run(PracticeApplication.class, args);
     }
 }
