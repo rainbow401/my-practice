f041e935bafa56759336b9662aefca419ab99df2
ZR21080178-闫志浩 <yanzhihao@zhongruigroup.com>
2022-08-05 16:14:08 +0800

    Update MailController.java

--- src/main/java/com/practice/domain/mail/MailController.java
+++ src/main/java/com/practice/domain/mail/MailController.java
@@ -1,20 +1,15 @@
 package com.practice.domain.mail;
 
 import com.practice.utils.EmailUtil;
-import org.apache.commons.io.FileUtils;
-import org.springframework.core.io.UrlResource;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.net.util.URLUtil;
-import sun.plugin2.message.helper.URLHelper;
 
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
 import java.io.File;
 import java.io.IOException;
-import java.net.*;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,18 +29,14 @@ public class MailController {
     }
 
     @PostMapping("/attachment/send")
-    public void sendEmailWithAttachment(String path, String fileUrl) throws MessagingException, URISyntaxException, IOException {
+    public void sendEmailWithAttachment(String path, String fileUrl) throws MessagingException, IOException {
         Map<String, Object> param = new HashMap<>();
         param.put("title", "你好");
         param.put("code", "abcd");
         Map<String, File> fileMap = new HashMap<>();
 
-
-        UrlResource resource = new UrlResource(fileUrl);
         fileMap.put("file1.png", new File("C:\\Users\\lenovo\\Desktop\\企业微信截图_16566577127765.png"));
         fileMap.put("file2.png", new File("C:\\Users\\lenovo\\Desktop\\企业微信截图_16566577127765.png"));
-
-
 
         emailUtil.sendEmailWithAttachments(
                 new String[]{"273697776@qq.com", "yanzhihao@zhongruigroup.com"},
