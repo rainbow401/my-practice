dcfce5c0d764d0fda6677e77538859ad4d1acc02
闫志浩 <yanzhihao@zhongruigroup.com>
2023-05-18 09:19:56 +0800

    update

---
+++ src/main/java/com/rainbow/practice/mysqlview/model/DbInfoSQL.java
@@ -0,0 +1,6 @@
+package com.rainbow.practice.mysqlview.model;/**
+ * 
+ * @author yanzhihao
+ * @since 2023/4/28
+ */public interface DbInfoSQL {
+}
---
+++ src/main/java/com/rainbow/practice/mysqlview/syntax/Test.java
@@ -0,0 +1,6 @@
+package com.rainbow.practice.mysqlview.syntax;/**
+ * 
+ * @author yanzhihao
+ * @since 2023/5/8
+ */public class Test {
+}
