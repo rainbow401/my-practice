783f650daab979dcc0c332cab000d9b6123056de
ZR21080178-闫志浩 <273697776@qq.com>
2023-07-01 18:13:35 +0800

    query param

--- src/main/java/com/rainbow/practice/http/HttpUtils.java
+++ src/main/java/com/rainbow/practice/http/HttpUtils.java
@@ -1,9 +1,5 @@
 package com.rainbow.practice.http;
 
-import cn.hutool.http.HttpUtil;
-import org.springframework.util.MultiValueMap;
-import sun.net.www.http.HttpClient;
-
 /**
  * @author: YanZhiHao
  * @date: 2023/3/3
